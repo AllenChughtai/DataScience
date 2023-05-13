aac6255b8ecacd0083d50e7b66c55e32b8949933
AllenChughtai <allen.chughtai@gmail.com>
2023-05-13 11:40:17 -0400

    Updates

--- Python/Test.py
+++ Python/Test.py
@@ -103,9 +103,69 @@ sex = 1
 
 new_insurance_cost = 250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
 change_in_insurance_cost = new_insurance_cost-insurance_cost
-print("The change in estimated insurance cost for beiung male instead of female "+ str(change_in_insurance_cost))
+print("The change in estimated insurance cost for being male instead of female "+ str(change_in_insurance_cost))
+
+#Functions
+
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+#Want to return max,min,round price of gift shop items
+gift_shop_list = [tshirt_price,shorts_price,mug_price,poster_price]
+max_price = (max(gift_shop_list))
+#max_price = (max(tshirt_price,shorts_price,mug_price,poster_price))
+min_price = (min(gift_shop_list))
+rounded_price = (round(tshirt_price,1))
+
+print(max_price)
+print(min_price)
+print(rounded_price)
+
+#Variable Access
+# Variable has been taken outside of a function and made global
+# This function will print a hardcoded count of how many locations we have.
+favorite_locations = "Paris, Norway, Iceland"
+
+def print_count_locations():
+  favorite_locations = "Paris, Norway, Iceland"
+  print("There are 3 locations")
+    
+# This function will print the favorite locations
+def show_favorite_locations():
+  print("Your favorite locations are: " + favorite_locations)
+
+print_count_locations()
+show_favorite_locations()
+
+# Returns
+current_budget = 3500.75
+shirt_expense = 9
 
 
+def deduct_expense(budget,expense):
+  return (budget-expense)
 
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+print_remaining_budget(current_budget)
 
+new_budget_after_shirt = deduct_expense(current_budget,shirt_expense)
 
+print_remaining_budget(new_budget_after_shirt)
+
+# Mutiple Returns
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first,second,third
+
+#The return values are bing associated to the following variables.
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(top_tourist_locations_italy())
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
